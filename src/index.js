1a71a979f057e7469ec7339399ac8fe3ce7e675e
Rommel Rico <me@rommelrico.com>
2020-06-29 06:56:24 -0700

    :sparkles: API: Configuring mongo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import ENV from "../env";
-
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const mongoUri = 'mongodb+srv://rommel:REDACTED@cluster0-bjpcs.mongodb.net/dev?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance!');
+});
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
   res.send('hi there');
