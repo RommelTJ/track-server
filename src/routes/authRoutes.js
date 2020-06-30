20359e24accc350c81ccd5dffa70779d85a6c8b0
Rommel Rico <me@rommelrico.com>
2020-06-30 06:48:01 -0700

    :sparkles: API: Handling JSON data

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 router.post('/signup', (req, res) => {
+  console.log("body", req.body);
   res.send('You made a post request.');
 });
 
