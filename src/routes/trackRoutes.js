d69f1a625116b52b3ef978b18f8719de66780f1e
Rommel Rico <me@rommelrico.com>
2020-07-03 12:54:10 -0700

    :sparkles: API: Creating Tracks

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -12,4 +12,17 @@ router.get('/tracks', async (req, res) => {
   res.send(tracks);
 });
 
+router.post('/tracks', async (req, res) => {
+  const { name, locations } = req.body;
+  if (!name || !locations) return res.status(422).send({error: "You must provide a name and locations."});
+
+  try {
+    const track = new Track({ userId: req.user._id, name, locations });
+    await track.save();
+    res.send(track);
+  } catch (e) {
+    res.status(422).send({ error: e.message });
+  }
+});
+
 module.exports = router;
