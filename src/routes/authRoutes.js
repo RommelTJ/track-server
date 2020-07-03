8efe7f664ddca9f691e18f428d898f6c95c3be69
Rommel Rico <me@rommelrico.com>
2020-07-03 12:27:08 -0700

    :sparkles: API: The SignIn Route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -19,4 +19,18 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) return res.status(422).send({error: "Must provide email and password."});
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).send({error: "Email not found."});
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, jsonSecretKey);
+    res.send({ token });
+  } catch (e) {
+    return res.status(422).send({ error: 'Something went wrong: ' + e.message});
+  }
+});
+
 module.exports = router;
