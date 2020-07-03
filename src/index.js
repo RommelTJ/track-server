30e830d6aec3d126b4ca9d57116827008a1915e9
Rommel Rico <me@rommelrico.com>
2020-07-03 11:25:29 -0700

    :sparkles: API: Wiring up JSON Web Tokens

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 app.use(bodyParser.json());
@@ -21,8 +22,8 @@ mongoose.connection.on('error', (err) => {
   console.error('Error connecting to mongo', err);
 });
 
-app.get('/', (req, res) => {
-  res.send('hi there');
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
