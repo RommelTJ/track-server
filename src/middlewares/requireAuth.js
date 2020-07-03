1ebb3d2b448425bc71da6e6fa61b1b77372b371a
Rommel Rico <me@rommelrico.com>
2020-07-03 11:23:33 -0700

    :tada: API: requireAuth middleware

---
+++ src/middlewares/requireAuth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const jsonSecretKey = require('../../env');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).send({ error: "You must be logged in." });
+
+  const token = authorization.replace('Bearer ', '');
+  jwt.verify(token, jsonSecretKey, async (err, payload) => {
+    if (err) return res.status(401).send({ error: "You must be logged in." });
+
+    const { userId } = payload;
+    req.user = await User.findById(userId);
+    next();
+  });
+};
